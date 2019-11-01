828ce5b3637e57bb69c68f76c92894d0007f5b02
sajid7tech <55694384+sajid7tech@users.noreply.github.com>
2019-11-01 21:57:02 +0530

    Add files via upload
    linked list program of all operation 
    last delete from nth funtion is missing

--- linkedlist_of_all_opr.c
+++ linkedlist_of_all_opr.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 struct node{
 	int data;
@@ -12,23 +13,24 @@ void insert_beg(void);
 void insert_end(void);
 void insert_nth(void);
 
-//void delete_beg(void);
-//void delete_end(void);
-//void delete_nth(void);
+void delete_beg(void);
+void delete_end(void);
+void delete_nth(void);
 
 void display(void);
 
 
 int main()
 {
-	head=NULL; // here we making aur list empty
+	head=NULL; // here we making our list empty
 	int choice;
 	while(1)
 	{
-		printf("\n\nImplementation of single linked lists\n");
-		printf("\n\n1.insert at Begining\n2.insert at End\n3.insert at nth Position\n");
+		printf("\n*********************Implementation of single linked lists**********************\n");
+		printf("\n1.insert at Begining\n2.insert at End\n3.insert at nth Position\n");
 		printf("4.delete at begining\n5.delete at end\n6.delete at nth position\n7.display\n8.EXIT\n\n");
-		scanf("%d\n",&choice);
+		printf("Enter your choice :");
+		scanf("%d",&choice);
 		switch(choice)
 		{
 			case 1: insert_beg();
@@ -37,12 +39,12 @@ int main()
 					break;
 			case 3: insert_nth();
 					break;
-			//case 4: delete_beg();
-					//break;
-			//case 5: delete_end();
-					//break;
-			//case 6: delete_nth();
-					//break;
+			case 4: delete_beg();
+					break;
+			case 5: delete_end();
+					break;
+			case 6: delete_nth();
+					break;
 			case 7: display();
 					break;
 			case 8: exit(1);
@@ -86,7 +88,7 @@ void insert_end()
 	int x;
 	struct node* temp;
 	struct node* ptr;
-	ptr = (struct node*)malloc(sizeof(struct node));
+		ptr = (struct node*)malloc(sizeof(struct node));
 		printf("\nEnter the number to be added at the end of the list\n");
 		scanf("%d",&x);
 	if(ptr==NULL)
@@ -108,9 +110,9 @@ void insert_end()
 		{
 			temp=temp->next;		
 		}
-		temp->next=ptr;
 		ptr->data=x;
 		ptr->next=NULL;
+		temp->next=ptr;
 	}
 }
 
@@ -118,22 +120,23 @@ void insert_end()
 void insert_nth()
 {
 	
-	int x,n;
+	int x,n,i;
 	struct node* ptr;
+	struct node* temp1,*temp2;
+	//struct node* temp2;
 	ptr = (struct node*)malloc(sizeof(struct node));
-		printf("\nEnter the number to be added at the end of the list\n");
-		scanf("%d",&x);
-		printf("Enter the position to where the element to be enter");
+
+		printf("Enter the position to where the element to be inserted\n");
 		scanf("%d",&n);
-	if(n==1)
-	{	
-		insert_beg();
-	}
+		printf("\nEnter the number to the list\n");
+		scanf("%d",&x);
+
 	if(ptr==NULL)
 	{
 		printf("Warning : Memory is full !!");
 	}
-	else if(head==NULL)
+	
+	if(n==1)
 	{
 		
 		ptr->data=x;
@@ -142,46 +145,99 @@ void insert_nth()
 	}
 	else
 	{
-	
+		temp1=head;
+
+		for(i=1;i<n-1;i++)
+		{
+			temp1=temp1->next;	
+		}
+
+		ptr->data=x;
+		temp2=temp1->next;
+		ptr->next=temp2;
+		temp1->next=ptr;git tutorial in hindi
+		
 	}
+	
+}
+
+
+void delete_beg()
+{
+	struct node *temp;
+	temp=head;
+	if(temp==NULL)
+	{
+		printf("List is empty");
+	}
+	else
+	{
+		head=temp->next;
+		free(temp);
+	}
+		
+}
+
+
+void delete_end()
+{
+		struct node* temp;.
+		temp=head;
+		if(head==NULL)
+		{
+			printf("List is Empty nothing to delete");
+		}
+		else
+		{
+			while(temp->next!=NULL)
+			{
+				temp=temp->next;
+			}
+			
+		}
+	
+}
+
+void delete_nth()
+{
+	struct node *temp1,*temp2; 
+	int n;
+	printf("from which position do you what to remove the element");
+	scanf("%d",&n);
+	while(temp->next!=NULL)
+			{
+				temp1=temp1->next;
+			}
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 void display()
 {
-	struct node* temp;
+	struct node * temp;
 	temp=head;
-	while(temp->next!=NULL)
+	printf("\n##########################\n\n");
+	while(temp!=NULL)
 	{
-		printf("\n##########################\n");
 		printf("Element is : %d\n",temp->data);
-		printf("\n##########################\n");
 		temp=temp->next;
 	}
-	
+	printf("\n##########################\n");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
